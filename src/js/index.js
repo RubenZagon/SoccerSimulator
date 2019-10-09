edba9014d36371b08f9dc5ac5f57d6a0db7aa1fc
Ruben <ruben.zagon@gmail.com>
2019-10-09 17:35:11 +0100

    Pasamos a hacer que el botón otro partido funcione

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,47 @@
 /**
  * OBJETIVO: Simulador de partidos de fútbol y sus resultados.
- * 
+ *
  *  - Elegir dos equipos aleatorios de una lista
  *  - Asignar resultados al partido disputado
  *  - Requisitos: Asegurar equipos sean diferentes
  */
 
+
+const marcador = document.querySelector('.marcador')
+const boton = document.querySelector('.btnPartido')
+
 const TEAMS = ['Real Madrid', 'Barcelona', 'Tenerife', 'Gran Canaria']
 
 
 class Equipo {
-    constructor(t ="", p = 0){
+    constructor(t = "", p = 0) {
         this.team = t
         this.points = p
     }
+
+    getTeam() {
+        return Equipo.team
+    }
+
+    getPoints() {
+        return Equipo.points
+    }
+
 }
 
 const getRandomNumber = (numb) => {
     return Math.floor(Math.random() * numb);
-} 
+}
 
 const generateTeams = () => {
     let equipos = []
 
-    for (i = 0; i < 2; i++){
+    for (i = 0; i < TEAMS.length; i++) {
 
         //Necesito generar un número aleatorio con lo ancho del array de equipos, luego en cada pasada del for, ir añadiendo ese equipo con una puntuación random y quitandolo del array - Tal vez utilizar .filter
 
-        equipo = new Equipo (TEAMS[getRandomNumber(TEAMS.length)], getRandomNumber(5))
-//        if (equipos != 'equipos')  //Aqui poner algo para buscar que no esté ya en el array de dentro
+        equipo = new Equipo(TEAMS[getRandomNumber(TEAMS.length)], getRandomNumber(5))
+        //        if (equipos != 'equipos')  //Aqui poner algo para buscar que no esté ya en el array de dentro
 
         equipos.push(equipo)
     }
@@ -37,10 +50,14 @@ const generateTeams = () => {
 
 let Equipazos = generateTeams()
 
+/*
 console.table(generateTeams())
 console.log(Equipazos[0].team)
+*/
 
-const marcador = document.querySelector('.marcador')
+const setScoreboard = (array) => {
+    console.log('apretar boton')
+    return array[0].team + "  " + array[0].points + "  -  " + array[1].team + "  " + array[1].points
+}
 
-
-marcador.innerHTML = Equipazos[0].team + "  " + Equipazos[0].points + "  -  " + Equipazos[1].team + "  " + Equipazos[1].points
+boton.addEventListener('click', marcador.innerHTML = setScoreboard(Equipazos));
