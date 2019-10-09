4e45458a8bc61bf775156c269e230726460ee8a6
Rubén Zagon <ruben.zagon@gmail.com>
2019-10-09 01:42:06 +0100

    Me falta poder comparar los equipos que no sean repetidos

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+/**
+ * OBJETIVO: Simulador de partidos de fútbol y sus resultados.
+ * 
+ *  - Elegir dos equipos aleatorios de una lista
+ *  - Asignar resultados al partido disputado
+ *  - Requisitos: Asegurar equipos sean diferentes
+ */
+
+const TEAMS = ['Real Madrid', 'Barcelona', 'Tenerife', 'Gran Canaria']
+
+
+class Equipo {
+    constructor(t ="", p = 0){
+        this.team = t
+        this.points = p
+    }
+}
+
+const getRandomNumber = (numb) => {
+    return Math.floor(Math.random() * numb);
+} 
+
+const generateTeams = () => {
+    let equipos = []
+
+    for (i = 0; i < 2; i++){
+
+        //Necesito generar un número aleatorio con lo ancho del array de equipos, luego en cada pasada del for, ir añadiendo ese equipo con una puntuación random y quitandolo del array - Tal vez utilizar .filter
+
+        equipo = new Equipo (TEAMS[getRandomNumber(TEAMS.length)], getRandomNumber(5))
+//        if (equipos != 'equipos')  //Aqui poner algo para buscar que no esté ya en el array de dentro
+
+        equipos.push(equipo)
+    }
+    return equipos
+}
+
+console.table(generateTeams())
